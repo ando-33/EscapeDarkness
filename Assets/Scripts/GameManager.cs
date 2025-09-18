fdc1347da13e30ecd7580af5b0a6ee7f983880f3
DELL-ando <aguguru81@gmail.com>
2025-09-18 16:25:15 +0900

    カメラコントローラーまで

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,17 +12,26 @@ public enum GameState
 
 public class GameManager : MonoBehaviour
 {
-    static public bool hasSpotLight; //スポットライトを持っているかどうか
+    public static GameState gameState; //ゲームのステータス
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static bool[] doorsOpenedState; //ドアの開閉状況
+
+    public static int key1;
+    public static int key2;
+    public static int key3;
+    public static bool[] keysPickedState; //鍵の取得状況
+
+    public static int bill = 10; //お札の残数
+    public static bool[] itemsPickedState; //アイテムの取得状況
+
+    public static bool hasSpotLight; //スポットライトを持っているかどうか
+
+    public static int playerHP = 3; //プレイヤーのHP
+
     void Start()
     {
-
+        //まずはゲームは開始状態にする
+        gameState = GameState.playing;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
